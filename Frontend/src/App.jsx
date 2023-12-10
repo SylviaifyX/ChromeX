096cffe9a825eac037436fc7007291139b52cf06
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-12-10 02:00:07 +0100

    files

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Auth from "./Layout/Auth/auth"
+function App() {
+  return (
+    <div className='bg-red-500'>
+     <Auth/>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/Layout/Auth/auth.jsx
@@ -0,0 +1,10 @@
+
+
+function Auth(){
+    return(
+        <>
+        hi
+        </>
+    )
+}
+export default Auth;
